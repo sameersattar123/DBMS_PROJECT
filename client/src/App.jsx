4d2a246daaa98bf4044243f7408914843cacdfdc
Sameer <sameersattar1111@gmail.com>
2024-06-28 01:18:17 +0500

    some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,9 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Books from "./Pages/Books";
-import UpdateBook from "./Pages/UpdateBook";
-import AddBook from "./Pages/AddBook";
+import React from 'react'
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Books />} />
-          <Route path="/addBook" element={<AddBook />} />
-          <Route path="/updateBook/:id" element={<UpdateBook />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
--- server/index.js
+++ server/index.js
@@ -18,41 +18,7 @@ const db = mysql.createConnection({
   database: "test_schema",
 });
 
-app.get("/books", (req, res) => {
-  const q = "SELECT * FROM test_schema.books";
-  db.query(q, (error, data) => {
-    if (error) {
-      console.log(error);
-    } else {
-      res.json(data);
-    }
-  });
-});
-app.post("/books", (req, res) => {
-  const q = "INSERT INTO books(`title`, `price` , `desc`, `cover`) VALUES (?)";
 
-  const values = [
-    req.body.title,
-    req.body.price,
-    req.body.desc,
-    req.body.cover,
-  ];
-
-  db.query(q, [values], (err, data) => {
-    if (err) return res.send(err);
-    return res.json("Book created successfully");
-  });
-});
-
-app.delete("/books/:id", (req, res) => {
-  const bookId = req.params.id;
-  const q = "DELETE FROM books WHERE id =?";
-
-  db.query(q, [bookId], (err, data) => {
-    if (err) return res.send(err);
-    return res.json("book delete successfully");
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`server is running`);
